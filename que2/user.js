f1a0097fa0efafc7f73f16e70870dc7726fbf4a0
PiyushJagtap97 <93431308+PiyushJagtap97@users.noreply.github.com>
2022-01-07 20:03:27 +0530

    Add files via upload

---
+++ que2/user.js
@@ -0,0 +1,47 @@
+const Promise = require("bluebird");
+const mysql = require("mysql");
+Promise.promisifyAll(require("mysql/lib/Connection").prototype);
+
+const dbinfo = {
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "project1",
+};
+
+
+const selectAllMsg = async() => {
+    const connection = mysql.createConnection(dbinfo); 
+    await connection.connectAsync();
+    console.log("server started");
+    
+    let sql = `select * from message`;
+    const list = await connection.queryAsync(sql);
+    console.log(list);
+
+    await connection.endAsync();
+    return list;
+};
+
+
+const addMsg = async (user) => {
+    const connection = mysql.createConnection(dbinfo);
+
+    await connection.connectAsync();
+
+    let sql = `insert into message (msg_record) values (?)`;
+    connection.queryAsync(sql, [user.msg_record]);
+    console.log("Message added");
+
+    await connection.endAsync();
+}
+
+module.exports = { selectAllMsg, addMsg };
+
+selectAllMsg();
+
+const user = {
+    msg_record:"Hello world...! How are you..?"
+}
+
+addMsg(user);
